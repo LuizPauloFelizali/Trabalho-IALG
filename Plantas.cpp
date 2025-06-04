63c834c33b06f7945581a60d2526e5671b84d48b
Luiz Paulo Felizali <luizpaulofelizali@gmail.com>
2025-06-04 16:05:12 -0300

    Implementação de bibliotecas e apresentação para usuário

--- Plantas.cpp
+++ Plantas.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-using namespace std;
-int main(){
+#include <fstream>
+#include <iomanip>
+#include <cstring>
 
-    cout << "exemplo";
+using namespace std;
+
+int main(){
+    
+    cout << "\n           JARDIM BOTANICO DIGITAL" << endl;
+    cout << "         Sistema de Cadastro de Plantas\n" ;
+
+    cout <<  " MENU PRINCIPAL \n" ;
+
+    cout << " [01]" << " Inserir nova planta\n";
+    cout << " [02]" << " Remover planta\n";
+    cout << " [03]" << " Buscar planta por ID\n";
+    cout << " [04]" << " Buscar planta por nome\n";
+    cout << " [05]" << " Mostrar todas as plantas\n";
+    cout << " [06]" << " Mostrar intervalo de plantas\n";
+    cout << " [07]" << " Ordenar plantas por ID\n";
+    cout << " [08]" << " Ordenar plantas por nome\n";
+
+    cout << " [09]" << " Importar do CSV\n";
+    cout << " [10]" << " Exportar para CSV\n";
+    cout << " [11]" << " Salvar alterações\n";
+
+    cout << " [00]" << " Sair\n";
+
+    cout << "\nDigite sua opcao: ";
+
+
 
     return 0;
 }
